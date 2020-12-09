2f949e4d406feb9b5172151849cf0edea2f7df6d
“ivonaionitagit config --global user.email “ivona_930@yahoo.com <75522357+ivonaionita@users.noreply.github.com>
2020-12-09 13:02:04 +0200

    Tema Lab 9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,35 @@ package com.company;
 
 public class Main {
 
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Ivona");
+
+        byte no1=1+2;
+        System.out.println(no1);
+
+        float no2= 3;
+        float no3=2;
+        System.out.println(no2/no3);
+        //float no2=(float)3/2;
+        //System.out.println(no2);
+
+        byte op1=-5+8*6;
+        System.out.println(op1);
+
+        byte op2= 55+9%9;
+        System.out.println(op2);
+
+        float op3=20;
+        float op4=(float)(-3)*5/8;
+        System.out.println(op3+op4);
+        //float op3= 20+(float)(-3)*5/8;
+        //System.out.println(op3);
+
+        float op5=5+15/3*2-8%3;
+        System.out.println(op5);
+
     }
 }
+
